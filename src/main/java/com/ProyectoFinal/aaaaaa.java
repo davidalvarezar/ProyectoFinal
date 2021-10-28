2a212a467c979d0f3c93c11d3e114de9db741c2f
David Alvarez <davidalvarez_4@hotmail.com>
2021-10-28 20:59:34 -0300

    prueba

---
+++ src/main/java/com/ProyectoFinal/aaaaaa.java
@@ -0,0 +1,14 @@
+package com.ProyectoFinal;
+
+public class aaaaaa {
+
+	private String david;
+
+	public String getDavid() {
+		return david;
+	}
+
+	public void setDavid(String david) {
+		this.david = david;
+	}
+}
